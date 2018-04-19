94cb4f137203ffce4005665e6da3d50165ba179d
Pegah Eizadkhah <peizadkh@gmail.com>
2018-04-18 19:34:33 -0700

    cleaned up tests/comments.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-e strict';
+'use strict';
 
 const YOUTUBE_SEARCH_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
 
@@ -8,37 +8,22 @@ function handleForm() {
   
   youtubeForm.on('submit', function(e) {
     e.preventDefault();
-    
     // get the usersearch entered
-     
     const query  = searchField.val();
-    //console.log(query);
-    /*
-    const query = {
-    part: 'snippet',
-    key: `AIzaSyAKFCsbsJ-lR3nhd4OMAyGh2YOO2KVtAC0`,
-    maxResults: 5
-  }
-    */
     // pass it in along with the YT endpoint
     fetchYTData(YOUTUBE_SEARCH_ENDPOINT, query);
-    
     // reset the input
     searchField.val('');
   });
 }
 
-
-function fetchYTData(baseURL, query) {
-  
+function fetchYTData(baseURL, query) { 
   // make the complete url by concatenating
   // the endpoint and username together
-//https://www.googleapis.com/youtube/v3/search?q=surfing&maxResults=2&key=AIzaSyAKFCsbsJ-lR3nhd4OMAyGh2YOO2KVtAC0&part=snippet
   const queryconcat = baseURL+"?q="+query+"&maxResults=5&key=AIzaSyAKFCsbsJ-lR3nhd4OMAyGh2YOO2KVtAC0&part=snippet";
   // try to get some JSON
   console.log(queryconcat);
-  // and show something to the user.
-  
+  // and show something to the user. 
   $.getJSON(queryconcat, renderData)
   // ... and show an error if we can't.
     .fail(showErr);
@@ -50,20 +35,15 @@ function renderData(data){
 
 function showResults(resultData) {
   // store the element we'll be appending to
-  console.log("!!!!!!");
-
   const outputElem = $('.js-output');
-  
   // Store the parts we want from data
   // using object destructuring
   let videoId = resultData.id.videoId;
   let title = resultData.snippet.title;
   let thumbnail = resultData.snippet.thumbnails.medium.url;
-  
   // if there's no name in the profile
   // well inform our user
   if (!title) title = 'undefined :(';
-  
   // We'll use the variables above to present
   // the information we got from youtube.
   let queryInfoHTML = "";
@@ -73,15 +53,11 @@ function showResults(resultData) {
       <h2><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">${title}</a></h2>
     </div>`
   );
-  
-  console.log(queryInfoHTML);
-  
   // then empty the output region
   // and append our profile info
   outputElem 
     .prop('hidden', false)
     .append(queryInfoHTML);
-
 }
 
 function showErr(err) {
@@ -97,8 +73,3 @@ function showErr(err) {
 }
 
 $(handleForm);
-
-
-//**************************************************
-
-
